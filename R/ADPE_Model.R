f4dd63b52826f1ddcd664d49298cd52d96e9d06b
Alexandra Strang <alexandra.strang@pg.canterbury.ac.nz>
2025-03-10 11:08:18 +1300

    Revision updates to model code
    Test if site effect is supported in full model and treat aspect as categorical variable

--- R/ADPE_Model.R
+++ R/ADPE_Model.R
@@ -1,5 +1,5 @@
 
-# Model to predict the number of breeding pairs of Adélie penguins using the area of guano for four colonies in the Ross Sea extracted from VHR imagery over 2009-2021
+# Model to translate the number of breeding pairs of Adélie penguins to the area of guano of four colonies in the Ross Sea extracted from VHR imagery over 2009-2021
 # Creator: Alexandra Strang
 
 sessionInfo() # for citing package versions
@@ -50,7 +50,7 @@ AIC(New.model)
 
 library(MuMIn) # for AICc scores
 
-BP.AICc <- AICc(New.model)
+AICc(New.model)
 
 #####################################################################################################################
 # View relationship between BP and GA
@@ -298,73 +298,6 @@ spatial_test <- testSpatialAutocorrelation(resids_aggregated, x = coordinates_ag
 print(spatial_test)
 # spatial autocorrelation not statistically significant
 
-# Account for repeated measures within a site with linear mixed-effect model (LMM) that includes site as a random effect
-
-library(lme4)
-
-# LMM with site effect using REML
-lmm1 <- lme4::lmer(logGuano_area ~ logBP + (1|Site_ID), data = Dataset.5.2)
-
-anova(lmm1) # just looks at fixed effects
-summary(lmm1) # variances came from here
-
-# Compare LMM to LM using ML method
-lmm2 <- lme4::lmer(logGuano_area ~ logBP + (1|Site_ID), REML = FALSE, data = Dataset.5.2)
-
-anova(lmm2) # just looks at fixed effects
-summary(lmm2)
-
-# using REML = FALSE (or ML method) gives below error
-# boundary (singular) fit: see help('isSingular')
-# no spatial autocorrelation? - removes variance from site effect due over paramitisation
-
-AIC(New.model, lmm2)
-# model without site effect better
-
-AICc(New.model, lmm2)
-
-BP.AICc <- AICc(New.model) # parsimonious model 
-lmm2.AICc <- AICc(lmm2) # model with site effect
-
-# Delta AICc and Akaike weights calculated on line 492
-# due to package mask between qpcr and MuMin
-
-# View residuals of LMM with site effect
-ds4$fitted2 <- fitted(lmm1)
-ds4$resid2 <- resid(lmm1)
-
-ds4$Colony <- as.factor(ds4$Site_ID)
-
-# Plot
-lmm1.resids <- ggplot(ds4, aes(x=fitted2, y=resid2, colour = Colony, fill = Colony, shape = Colony)) +
-  geom_point(size=3 )+
-  geom_hline(yintercept=0) +
-  xlab("Observed") +
-  ylab("Residuals") +
-  scale_shape_manual(values = c(21,21,21,21))+
-  scale_fill_manual(values = colours) +
-  scale_colour_manual(values = colours) +
-  theme_classic()+
-  theme(axis.text.x = element_text(color="black", size=12),
-        axis.text.y = element_text(color="black", size=12),)+
-  scale_y_continuous(limits = c(-0.4,0.6), breaks = seq(-0.4,0.6, by=0.2))
-
-
-lmm1.resids # Plot not presented in manuscript
-
-# Plot together
-plot(ggarrange(New.model.resids, 
-               lmm1.resids, 
-               common.legend = TRUE,
-               legend = "right",
-               ncol = 1, nrow = 2, labels=c("a","b")))
-
-# Plot not presented in manuscript
-
-# look very similar
-# a is LM without site effect
-# b is LMM with site effect
-
 #####################################################################################################################
 # Investigate temporal autocorrelation in the residuals
 #####################################################################################################################
@@ -381,16 +314,28 @@ acf(temporal.residuals) # no temporal autocorrelation
 # Investigate factors that may influence relationship
 #####################################################################################################################
 
-# Calculate northness (use northness instead of aspect due to aspect's circular nature in models)
+# Treat aspect as a categorical variable with 8 cardinal directions
 
-# Convert degrees to radians
-Dataset.5.0$AspectRadians <- pi*Dataset.5.0$Aspect/180
+library(dplyr)
 
-# Calculate northness
-Dataset.5.0$Northness <- cos(Dataset.5.0$AspectRadians)
+# Categorizing aspect values into 8 directions
+Dataset.5.0 <- Dataset.5.0 %>%
+  mutate(Aspect_factor = case_when(
+    (Aspect >= 337.5 | Aspect < 22.5)  ~ "N",
+    (Aspect >= 22.5 & Aspect < 67.5)   ~ "NE",
+    (Aspect >= 67.5 & Aspect < 112.5)  ~ "E",
+    (Aspect >= 112.5 & Aspect < 157.5) ~ "SE",
+    (Aspect >= 157.5 & Aspect < 202.5) ~ "S",
+    (Aspect >= 202.5 & Aspect < 247.5) ~ "SW",
+    (Aspect >= 247.5 & Aspect < 292.5) ~ "W",
+    (Aspect >= 292.5 & Aspect < 337.5) ~ "NW",
+    TRUE ~ NA_character_
+  ))
 
-# Extracting extra needed variables
-Dataset.5.3 <- Dataset.5.0[,c("logBP", "logGuano_area", "Site_ID", "PAR", "Slope", "Northness")]
+View(Dataset.5.0)
+
+# Extracting only needed variables
+Dataset.5.3 <- Dataset.5.0[,c("logBP", "logGuano_area", "Site_ID", "Colony", "Slope", "PAR", "Aspect_factor")]
 
 # Remove NAs
 Dataset.5.4 <- na.omit(Dataset.5.3)
@@ -401,7 +346,7 @@ View(Dataset.5.4)
 
 library(corrplot)
 
-covariates <- data.frame(Dataset.5.4$logBP, Dataset.5.4$Slope, Dataset.5.4$Northness, Dataset.5.4$PAR)
+covariates <- data.frame(Dataset.5.4$logBP, Dataset.5.4$Slope, Dataset.5.4$PAR)
 
 cor.matrix <- cor(covariates) # default method is pearsons
 
@@ -409,25 +354,47 @@ corrplot(cor.matrix, method = "number", type = "lower", tl.cex = 1)
 
 # Plot not presented in manuscript
 
-# Can't use northness due to correlation with BP - Removed from further analysis
-# northness and BP correlated positively (0.96)
 # PAR and BP correlated positively (0.61)
-# northness and PAR correlated positively (0.46)
+# check categorical aspect with VIF
+
+#####################################################################################################################
+# Account for repeated measures within a site with linear mixed-effect model (LMM) that includes site as a random effect
+#####################################################################################################################
+
+# Test if site effect is supported in full model (most complex) with par and slope
+Full_model <- lme4::lmer(logGuano_area ~ logBP + PAR + Slope + (1|Site_ID), data = Dataset.5.4)
+
+anova(Full_model) # just looks at fixed effects
+summary(Full_model)
+# singular fit with very small random effect
+# doesn't explain any variance with site effect
+
+# Compare LMM to LM using ML method
+Full_model_ML <- lme4::lmer(logGuano_area ~ logBP + PAR + Slope + (1|Site_ID), REML = FALSE, data = Dataset.5.4)
+
+anova(Full_model_ML) # just looks at fixed effects
+summary(Full_model_ML)
+# singular fit with very small random effect
+
+# compare to LM with AIC and AICc under full model
+
+#####################################################################################################################
+# Run candidate models
+#####################################################################################################################
 
 library(car) # for VIF values
 
-# Run candidate models
-
-# Removed from further analysis - Candidate model with northness (correlated with BP)
-lm.GA.a1 <- lm(Dataset.5.4$logGuano_area ~ Dataset.5.4$logBP + Dataset.5.4$Northness)
+# Removed from further analysis - Candidate model with aspect (collinearity)
+lm.GA.a1 <- lm(Dataset.5.4$logGuano_area ~ Dataset.5.4$logBP + Dataset.5.4$Aspect_factor)
 
 summary(lm.GA.a1)
-vif(lm.GA.a1) # well above 2.0
+vif(lm.GA.a1) # BP above 2.0 (need to remove aspect)
 
 # removed BP
-lm.GA.a2 <- lm(Dataset.5.4$logGuano_area ~ Dataset.5.4$Northness)
+lm.GA.a2 <- lm(Dataset.5.4$logGuano_area ~ Dataset.5.4$Aspect_factor)
+# aspect confounded with colony size
 
-summary(lm.GA.a2) # northness does have an influence - not as good as BP
+summary(lm.GA.a2) # aspect does have an influence - but BP needs to be included
 
 # Candidate model with slope
 lm.GA.b <- lm(Dataset.5.4$logGuano_area ~ Dataset.5.4$logBP + Dataset.5.4$Slope)
@@ -439,23 +406,23 @@ vif(lm.GA.b) # both under 2.0
 AIC(New.model, lm.GA.b) # over fit? due to low sample size try AICc
 AICc(New.model, lm.GA.b) # model without slope better
 
-# Removed from further analysis - Candidate model with northness and slope (have to remove northness)
-lm.GA.c <- lm(Dataset.5.4$logGuano_area ~ Dataset.5.4$logBP + Dataset.5.4$Northness + Dataset.5.4$Slope)
+# Removed from further analysis - Candidate model with aspect and slope (have to remove aspect)
+lm.GA.c <- lm(Dataset.5.4$logGuano_area ~ Dataset.5.4$logBP + Dataset.5.4$Aspect_factor + Dataset.5.4$Slope)
 
 summary(lm.GA.c)
-vif(lm.GA.c) # only slope under 2.0
+vif(lm.GA.c) # all above 2.0
 
 # Candidate model with PAR (PAR and BP slightly correlated)
 lm.GA.d <- lm(Dataset.5.4$logGuano_area ~ Dataset.5.4$logBP + Dataset.5.4$PAR)
 
 summary(lm.GA.d)
 anova(lm.GA.d)
-vif(lm.GA.d) # both under 2.0 (Don't need to remove PAR)
+vif(lm.GA.d) # both under 2.0 (don't need to remove PAR)
 
 AIC(New.model, lm.GA.d) # model without PAR better
 AICc(New.model, lm.GA.d) # model without PAR better
 
-# Candidate model with PAR and slope
+# Candidate model with PAR and slope (full model)
 lm.GA.e <- lm(Dataset.5.4$logGuano_area ~ Dataset.5.4$logBP + Dataset.5.4$PAR + Dataset.5.4$Slope)
 
 summary(lm.GA.e)
@@ -464,6 +431,10 @@ vif(lm.GA.e) # all under 2.0
 
 AIC(New.model, lm.GA.e) # model without PAR and slope better
 AICc(New.model, lm.GA.e) # model without PAR and slope better
+
+# compare full model to full model with site effect
+AIC(lm.GA.e, Full_model_ML) 
+AICc(lm.GA.e, Full_model_ML) # model without site effect better
 
 # Compare AICc scores
 
@@ -475,6 +446,7 @@ Slope.AICc <- AICc(lm.GA.b) # slope model
 PAR.AICc <- AICc(lm.GA.d) # PAR model
 Slope.PAR.AICc <- AICc(lm.GA.e) # slope and PAR model
 null.AICc <- AICc(null.model) # intercept only model
+Full.AICc <- AICc(Full_model_ML) # full model
 
 BP.AICc-null.AICc
 BP.AICc-Slope.AICc
@@ -489,8 +461,8 @@ library(qpcR) # package masks MuMin so run after
 x <- c(BP.AICc, Slope.AICc, PAR.AICc, Slope.PAR.AICc, null.AICc)
 akaike.weights(x)
 
-# for site effect
-x <- c(BP.AICc, lmm2.AICc)
+# for site effect in full model
+x <- c(Slope.PAR.AICc, Full.AICc)
 akaike.weights(x)
 
 # Likelihood ratio test (results not reported in the manuscript)
@@ -499,17 +471,17 @@ library(lmtest)
 # start with the full model first
 
 # compare above model 1 to 2, 3, and 4
-lrtest(lm.GA.e, lm.GA.b) # 0.50
-lrtest(lm.GA.e, lm.GA.d) # 0.12
-lrtest(lm.GA.e, New.model) # 0.24
+lrtest(lm.GA.e, lm.GA.b) # 0.57
+lrtest(lm.GA.e, lm.GA.d) # 0.16
+lrtest(lm.GA.e, New.model) # 0.30
 # full model with slope and PAR is not an improvement
 
 # compare above model 2 to 4
-lrtest(lm.GA.b, New.model) # 0.12
+lrtest(lm.GA.b, New.model) # 0.15
 # full model with slope is not an improvement
 
 # compare above model 3 to 4
-lrtest(lm.GA.d, New.model) # 0.49
+lrtest(lm.GA.d, New.model) # 0.55
 # full model with PAR is not an improvement
 
 #####################################################################################################################
